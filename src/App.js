309a8a669cb81a5ba187d9a4ed433f48c9496fe2
AnuradhaJaya <176155511+anuradhajayathunga@users.noreply.github.com>
2025-04-29 08:06:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Authenticatio from './pages/Authentication/Authenticatio';
+
+function App() {
+  return (
+    <div className="">
+      <Authenticatio />
+    </div>
+  );
+}
+
+export default App;
